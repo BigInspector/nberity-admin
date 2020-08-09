52fe88353832a94a6a39c7dd48558e31969b129a
Karl Jürgenson <karl5@windowslive.com>
2020-08-10 01:56:02 +0300

    Initial commit

---
+++ src/main/java/com/nberity/NberityApplication.java
@@ -0,0 +1,13 @@
+package com.nberity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NberityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NberityApplication.class, args);
+    }
+
+}
